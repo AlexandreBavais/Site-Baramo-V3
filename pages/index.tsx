5449e6ef3d4452a975b9609a7c50904e9b6785f6
LucasRamadour <72853747+LucasRamadour@users.noreply.github.com>
2022-11-09 14:46:51 +0100

    Scroll navabr

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,22 @@ import SeoLottie from "../public/lottie/seo.json";
 import BlockLottie from "../public/lottie/block.json";
 import CreationCard from "../src/components/creationcard";
 import ContactSection from '../src/components/ContactSection';
+import { useRef } from 'react';
 
 export default function Home() {
+
+  const NavbarRef = useRef(null);
+  const HomeRef = useRef(null);
+  const WeAreRef = useRef(null);
+  const ServicesRef = useRef(null);
+  const CreationsRef = useRef(null);
+  const EquipeRef = useRef(null);
+  const ContactRef = useRef(null);
+
+  const scrollTo = (ref:any) => {
+    window.scrollTo({ top: ref.current.getBoundingClientRect().top + window.pageYOffset - (NavbarRef?.current?.clientHeight || 0 ), behavior: 'smooth' })
+  }
+
   return (
 
     <div className={tw`flex flex-1 w-full h-full min-h-[100vh] flex-col ${css({ backgroundImage: "url('/images/back.svg')" })}`}>
@@ -20,21 +34,21 @@ export default function Home() {
       </Head>
 
 
-      <div className={tw`flex w-full h-[10vh] items-center px-10 justify-between sticky top-0 ${css({ "backdrop-filter": "blur(25px)" })} z-20`}>
-        <div className={tw`flex flex-row items-center space-x-4`}> <img src="/images/logo.svg" alt="logo baramo" />
-          <p className={tw`font-medium text-xl text-black`}>BARAMO</p>
+      <div ref={NavbarRef} className={tw`flex w-full h-[10vh] items-center px-10 justify-between sticky top-0 ${css({ "backdrop-filter": "blur(25px)" })} z-20`}>
+        <div className={tw`flex flex-row items-center space-x-4 cursor-pointer`}> <img src="/images/logo.svg" alt="logo baramo" />
+          <p className={tw`font-medium text-xl text-black`} onClick={() => { scrollTo(HomeRef) }}>BARAMO</p>
         </div>
         <div className={tw`flex flex-row items-center space-x-10 text-black`}>
-          <div className={tw`cursor-pointer`}>WE ARE</div>
-          <div className={tw`cursor-pointer`}>SERVICES</div>
-          <div className={tw`cursor-pointer`}>CRÉATIONS</div>
-          <div className={tw`cursor-pointer`}>ÉQUIPE</div>
-          <div className={tw`text-[#9747FF] cursor-pointer`}>CONTACT</div>
+          <div className={tw`cursor-pointer`} onClick={() => { scrollTo(WeAreRef) }}>WE ARE</div>
+          <div className={tw`cursor-pointer`} onClick={() => { scrollTo(ServicesRef) }}>SERVICES</div>
+          <div className={tw`cursor-pointer`} onClick={() => { scrollTo(CreationsRef) }}>CRÉATIONS</div>
+          <div className={tw`cursor-pointer`} onClick={() => { scrollTo(EquipeRef) }}>ÉQUIPE</div>
+          <div className={tw`text-[#9747FF] cursor-pointer`} onClick={() => { scrollTo(ContactRef) }}>CONTACT</div>
         </div>
 
       </div>
 
-      <div className={tw`flex w-full h-[90vh] px-10 text-9xl items-center text-black`}>
+      <div ref={HomeRef} className={tw`flex w-full h-[90vh] px-10 text-9xl items-center text-black`}>
         <div className={tw`flex h-full items-end pb-10 `}> <img src="/images/scroll.svg" alt="barre de scroll" /> </div>
         <div className={tw`px-20 max-w-[60%]`}>
           <h1> WE BUILD</h1>
@@ -52,7 +66,7 @@ export default function Home() {
       <div className={tw`flex w-full pl-20 pt-20`}>
         <img src="/images/separateur.png" alt="" />
       </div>
-      <div className={tw`flex w-full h-[90vh] text-8xl items-center text-black`}>
+      <div ref={WeAreRef} className={tw`flex w-full h-[90vh] text-8xl items-center text-black`}>
 
         <div className={tw`absolute`}>
           <img className={tw`h-[90vh]`} src="/images/bulleback.svg" alt="" />
@@ -73,7 +87,7 @@ export default function Home() {
         <img src="/images/separateur.png" alt="" />
       </div>
 
-      <div className={tw`flex flex-col w-full h-[90vh] text-8xl justify-evenly text-black`}>
+      <div ref={ServicesRef} className={tw`flex flex-col w-full h-[90vh] text-8xl justify-evenly text-black`}>
         <div className={tw`px-20 max-w-[60%]`}>
           <h2>NOS SERVICES</h2>
           <p className={tw`text-base text-gray-500 mt-8`}> Lorem ipsum dolor sit amet, consectetur adipiscing elit dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
@@ -109,7 +123,7 @@ export default function Home() {
       <div className={tw`flex w-full pl-20 pt-20`}>
         <img src="/images/separateur.png" alt="" />
       </div>
-      <div className={tw`flex flex-col w-full h-[90vh] text-8xl justify-evenly text-black`}>
+      <div ref={CreationsRef} className={tw`flex flex-col w-full h-[90vh] text-8xl justify-evenly text-black`}>
 
         <div className={tw`absolute right-0`}>
           <img className={tw`h-[90vh]`} src="/images/bulleback2.svg" alt="" />
@@ -132,7 +146,13 @@ export default function Home() {
       <div className={tw`flex w-full pl-20 pt-20`}>
         <img src="/images/separateur.png" alt="" />
       </div>
-      <div className={tw`flex flex-col w-full h-[90vh] text-8xl justify-evenly text-black px-20`}>
+      <div ref={EquipeRef} className={tw`flex flex-col w-full h-[90vh] text-8xl justify-evenly text-black px-20`}>
+
+      </div>
+      <div className={tw`flex w-full pl-20 pt-20`}>
+        <img src="/images/separateur.png" alt="" />
+      </div>
+      <div ref={ContactRef} className={tw`flex flex-col w-full h-[90vh] text-8xl justify-evenly text-black px-20`}>
         <ContactSection/>
       </div>
     </div>
